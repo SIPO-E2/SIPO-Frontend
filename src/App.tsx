332385bc36cd9982869cbbfffef2ac6d7d79662c
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-15 10:06:49 -0700

    Adding the main content

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import "./global.css";
 
 function App() {
   return (
-    <div className="App flex">
+    <div className="flex min-h-screen flex-row bg-gray-100 text-gray-800">
       <Sidebar />
-      <main className="flex-1 p-8">
-        <h1>Welcome to the App!</h1>
-        <p>
-          fdsfsdfdsfdsafdsaffsdfdsfsdfsdffdsfsdfdsfsdfdsfdsfdsffdsffsdfsdfsff
-        </p>
+      <main className="main -ml-48 flex flex-grow flex-col p-4 transition-all duration-150 ease-in md:ml-0 ll">
+        <div className="flex h-full items-center justify-center bg-white text-center text-5xl font-bold shadow-md margin">
+          Content
+        </div>
       </main>
     </div>
   );
