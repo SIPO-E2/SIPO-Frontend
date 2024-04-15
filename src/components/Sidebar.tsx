843b1efb505fd3b7e5480ef3ef350b7c503ae3f3
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-15 05:26:35 -0700

    Update sidebar component with floating drop down

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,11 +27,11 @@ const Sidebar: React.FC = () => {
 
   return (
     <div
-      className={`transition-width h-full shadow-md bg-white absolute ${
+      className={`transition-width h-full shadow-md bg-white absolute p-3 ${
         collapsed ? "w-20" : "w-72"
       }`}
     >
-      <div className="p-4 flex items-center">
+      <div className="flex items-center justify-center mb-4">
         <img
           src={EncoraLogo}
           alt="Encora Logo"
@@ -39,48 +39,64 @@ const Sidebar: React.FC = () => {
         />
       </div>
       <ul className="list-unstyled">
-        <li className="p-2 hover:bg-gray-100">
+        <li className="p-2 hover:text-custom-color">
           <div
             onClick={toggleDropdown}
             className="flex justify-start items-center cursor-pointer"
           >
-            <FontAwesomeIcon icon={faHome} className="mr-2" />{" "}
-            {!collapsed && "Home"}
+            <FontAwesomeIcon icon={faHome} className="text-custom-color mr-2" />
+            {!collapsed && <span className="text-custom-color">Home</span>}
             {!collapsed && (
               <FontAwesomeIcon
                 icon={dropdownOpen ? faChevronUp : faChevronDown}
-                className="ml-auto"
+                className="ml-auto text-custom-color"
               />
             )}
           </div>
-          {dropdownOpen && (
+          {collapsed && dropdownOpen && (
+            <div className="floating-dropdown show">
+              <ul>
+                <li className="p-2 hover text-custom-color">Submenu 1</li>
+                <li className="p-2 hover text-custom-color">Submenu 2</li>
+                <li className="p-2 hover text-custom-color">Submenu 3</li>
+              </ul>
+            </div>
+          )}
+          {!collapsed && dropdownOpen && (
             <ul className="pl-4">
-              <li className="p-2 hover:bg-gray-100">Submenu 1</li>
-              <li className="p-2 hover:bg-gray-100">Submenu 2</li>
-              <li className="p-2 hover:bg-gray-100">Submenu 3</li>
+              <li className="p-2 hover text-custom-color">Submenu 1</li>
+              <li className="p-2 hover text-custom-color">Submenu 2</li>
+              <li className="p-2 hover text-custom-color">Submenu 3</li>
             </ul>
           )}
         </li>
-        <li className="p-2 hover:bg-gray-100 flex items-center justify-start">
-          <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
-          {!collapsed && "About"}
+        <li className="p-2 hover:text-custom-color flex items-center justify-start">
+          <FontAwesomeIcon
+            icon={faInfoCircle}
+            className="text-custom-color mr-2"
+          />
+          {!collapsed && <span className="text-custom-color">About</span>}
         </li>
-        <li className="p-2 hover:bg-gray-100 flex items-center justify-start">
-          <FontAwesomeIcon icon={faWrench} className="mr-2" />
-          {!collapsed && "Services"}
+        <li className="p-2 hover:text-custom-color flex items-center justify-start">
+          <FontAwesomeIcon icon={faWrench} className="text-custom-color mr-2" />
+          {!collapsed && <span className="text-custom-color">Services</span>}
         </li>
-        <li className="p-2 hover:bg-gray-100 flex items-center justify-start">
-          <FontAwesomeIcon icon={faAddressBook} className="mr-2" />
-          {!collapsed && "Contact"}
+        <li className="p-2 hover:text-custom-color flex items-center justify-start">
+          <FontAwesomeIcon
+            icon={faAddressBook}
+            className="text-custom-color mr-2"
+          />
+          {!collapsed && <span className="text-custom-color">Contact</span>}
         </li>
       </ul>
       <div className="absolute bottom-0 w-full">
         <button
           onClick={toggleSidebar}
-          className="w-full p-4 hover:bg-gray-200"
+          className="w-full p-4 hover:text-custom-color-200"
         >
           <FontAwesomeIcon
             icon={collapsed ? faAngleDoubleRight : faAngleDoubleLeft}
+            className="text-custom-color"
           />
         </button>
       </div>
