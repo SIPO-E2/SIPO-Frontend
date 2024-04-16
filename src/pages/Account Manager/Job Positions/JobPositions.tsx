fb2b0e39cf5bd355872077e331dada4db4766ae7
camilardgzm <camilaguadalupe1002@gmail.com>
2024-04-16 01:48:35 -0700

    component TableJobPosition created (style)

--- src/pages/Account Manager/Job Positions/JobPositions.tsx
+++ src/pages/Account Manager/Job Positions/JobPositions.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+
+import TableJobPositions from "../../../components/TableJobPositions";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+
 
 const JobPositions = () => {
-  return <div>JobPositions</div>;
+
+  return(
+    <>
+      <div className="w-full">
+
+        <div className="px-5 pt-4 d-flex mb-3">
+
+          <div className="p-2 me-auto">
+            <h1> Job Positions </h1>
+          </div>
+
+          <div className="p-2 flex items-center justify-center">
+            <button type="button" className="btn btn-primary"> Add Job Position </button>
+          </div>
+
+
+          <div className="p-2 flex items-center justify-center">
+            <button type="button" >
+              <FontAwesomeIcon icon={faFilter} />
+            </button>
+          </div>
+        </div>
+        <hr className="border-2 ml-10 mr-10 border-black-900"/>
+
+      </div>
+      <TableJobPositions/>
+
+    </>
+  )
 };
 
 export default JobPositions;
