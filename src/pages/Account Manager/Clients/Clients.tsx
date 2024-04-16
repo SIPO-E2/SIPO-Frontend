1ae37c653c669f26af9205c54707d598d98bd287
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-16 00:57:19 -0700

    Refactor Clients component to include a centered red background and update content

--- src/pages/Account Manager/Clients/Clients.tsx
+++ src/pages/Account Manager/Clients/Clients.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Clients = () => {
-  return <div>Clientssssss</div>;
+  return (
+    <div className="flex justify-center items-center w-90 h-[1500px] bg-red-500 mx-auto mt-10 mb-0">
+      Clientssssss
+    </div>
+  );
 };
 
 export default Clients;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-// tailwind.config.js
+// tailwind.config.ts
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       transitionProperty: {
         width: "width",
+      },
+      width: {
+        90: "90%", //  90% width for clients page
+      },
+      height: {
+        90: "90%", // 90% width for clients page
       },
     },
   },
