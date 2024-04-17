fb06af08322de6e43cfb6b58d5ce3579b338a978
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-17 02:44:41 -0700

    Add client data file with initial client information, but there is a problem with the toggle settings

--- src/pages/Account Manager/Clients/Clients.tsx
+++ src/pages/Account Manager/Clients/Clients.tsx
@@ -23,12 +23,105 @@ import "../../../Clients.css";
 import { Link } from "react-router-dom";
 import "../../../Cards.css";
 import { faC } from "@fortawesome/free-solid-svg-icons/faC";
+import clientes from "./Data/data";
 
 // Defining an interface for the checkbox states
 interface CheckboxStates {
   division: boolean;
   highGrowth: boolean;
 }
+
+interface ClientCardProps {
+  toggleSettings: () => void; // Aquí especificas que toggleSettings es una función que no retorna nada.
+  isSettingsOpen: boolean; // Y que isSettingsOpen es un boolean.
+}
+
+const ClientCards: React.FC<ClientCardProps> = ({
+  toggleSettings,
+  isSettingsOpen,
+}) => {
+  return clientes.map((client, index) => (
+    <div className="col-lg-4 col-md-12 col-sm-12 mb-4" key={index}>
+      <div className="job-card">
+        <div className="card-top">
+          <img
+            src={client.imagenURL}
+            alt="Company Logo"
+            className="company-logo"
+          />
+          <div className="settings" onClick={toggleSettings}>
+            <FontAwesomeIcon icon={faEllipsisVertical} />
+            {isSettingsOpen && (
+              <div className="floating-dropdown show cursor-pointer">
+                <ul>
+                  <li className="drop-down-text">
+                    <Link to="">
+                      <FontAwesomeIcon
+                        icon={faEye}
+                        className="drop-down-icon"
+                      />
+                      View
+                    </Link>
+                  </li>
+                  <li className="drop-down-text">
+                    <Link to="">
+                      <FontAwesomeIcon
+                        icon={faPen}
+                        className="drop-down-icon"
+                      />
+                      Edit
+                    </Link>
+                  </li>
+                  <li className="drop-down-text red">
+                    <Link to="">
+                      <FontAwesomeIcon
+                        icon={faTrash}
+                        className="drop-down-icon"
+                      />
+                      Delete
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="top-card-section">
+          <h4 className="title-card">{client.nombre}</h4>
+          <p className="subtitle-card">Joining date: 16 Apr 2024</p>
+          <p className="subtitle-card blue spacing-bottom">
+            <FontAwesomeIcon icon={faBriefcase} className="job-icons blue" />
+            {client.numberOfProjects} Projects
+          </p>
+        </div>
+
+        <hr className="custom-hr-card" />
+        <div className="detail-section">
+          <div className="detail-row">
+            <p className="subtitle-card">
+              <FontAwesomeIcon icon={faChartSimple} className="job-icons" />
+              &gt; {client.experience} year exp
+            </p>
+            <p className="subtitle-card">
+              <FontAwesomeIcon icon={faEarthAmericas} className="job-icons" />
+              {client.division}
+            </p>
+          </div>
+          <div className="detail-row">
+            <p className="subtitle-card">
+              <FontAwesomeIcon icon={faMoneyBill} className="job-icons" />
+              {client.money}
+            </p>
+            <p className="subtitle-card">
+              <FontAwesomeIcon icon={faUser} className="job-icons" />
+              {client.highGrowthClient ? "High-Growth" : "Regular"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
+};
 
 const Clients = () => {
   const [dropdown, setDropdown] = useState(false);
@@ -108,36 +201,10 @@ const Clients = () => {
       <hr className="custom-hr" />
       <div className="container-fluid">
         <div className="row">
-          <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
-            <div className="card m-2">
-              <div className="card-body">Componente 1</div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
-            <div className="card m-2">
-              <div className="card-body">Componente 2</div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
-            <div className="card m-2">
-              <div className="card-body">Componente 3</div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
-            <div className="card m-2">
-              <div className="card-body">Componente 4</div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
-            <div className="card m-2">
-              <div className="card-body">Componente 5</div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-12 col-sm-12 mb-4">
-            <div className="card m-2">
-              <div className="card-body">Componente 6</div>
-            </div>
-          </div>
+          <ClientCards
+            toggleSettings={toggleSettings}
+            isSettingsOpen={isSettingsOpen}
+          />
         </div>
       </div>
     </div>
---
+++ src/pages/Account Manager/Clients/Data/data.tsx
@@ -0,0 +1,70 @@
+const clientes = [
+  {
+    imagenURL:
+      "https://api-prod-minimal-v510.vercel.app/assets/images/company/company_1.png",
+    nombre: "The North Face",
+    joiningDate: "16 Apr 2024",
+    numberOfProjects: 5,
+    experience: "5 years",
+    money: "$120,000",
+    highGrowthClient: true,
+    division: "Mexico",
+  },
+  {
+    imagenURL:
+      "https://api-prod-minimal-v510.vercel.app/assets/images/company/company_2.png",
+    nombre: "NOSE",
+    joiningDate: "16 Apr 2024",
+    numberOfProjects: 3,
+    experience: "3 years",
+    money: "$90,000",
+    highGrowthClient: false,
+    division: "Brazil",
+  },
+  {
+    imagenURL:
+      "https://api-prod-minimal-v510.vercel.app/assets/images/company/company_3.png",
+    nombre: "Wix",
+    joiningDate: "16 Apr 2024",
+    numberOfProjects: 8,
+    experience: "10 years",
+    money: "$200,000",
+    highGrowthClient: true,
+    division: "Francia",
+  },
+  {
+    imagenURL:
+      "https://api-prod-minimal-v510.vercel.app/assets/images/company/company_4.png",
+    nombre: "Coca Cola",
+    joiningDate: "16 Apr 2024",
+    numberOfProjects: 2,
+    experience: "1 year",
+    money: "$50,000",
+    highGrowthClient: false,
+    division: "USA",
+  },
+  {
+    imagenURL:
+      "https://api-prod-minimal-v510.vercel.app/assets/images/company/company_5.png",
+    nombre: "Tik Tok",
+    joiningDate: "16 Apr 2024",
+    numberOfProjects: 6,
+    experience: "8 years",
+    money: "$150,000",
+    highGrowthClient: true,
+    division: "Argentina",
+  },
+  {
+    imagenURL:
+      "https://api-prod-minimal-v510.vercel.app/assets/images/company/company_6.png",
+    nombre: "Best Buy",
+    joiningDate: "16 Apr 2024",
+    numberOfProjects: 6,
+    experience: "8 years",
+    money: "$150,000",
+    highGrowthClient: true,
+    division: "Argentina",
+  },
+];
+
+export default clientes;
