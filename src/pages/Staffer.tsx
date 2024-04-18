9009a0a491dfd18155bb01120a99ae00b86155fc
umakossiooo <a01254619@tec.mx>
2024-04-17 22:27:37 -0700

    fix: fixed dropdown so it can be fully visible

--- src/pages/Staffer.tsx
+++ src/pages/Staffer.tsx
@@ -55,7 +55,7 @@ const Staffer = (props: Props) => {
 
     return (
         <>
-            <div className="relative overflow-x-auto sm:rounded-lg p-4 z-3">
+            <div className="relative sm:rounded-lg p-4 z-3">
                 <table className=" w-full text-sm  rtl:text-right text-gray-500 dark:text-gray-400 shadow-md rounded">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
