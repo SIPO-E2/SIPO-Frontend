72be73ba95bfa42e9704e42574d7d17f73fbe9d9
marianaagrcg <marianagarciagomez2@gmail.com>
2024-04-17 17:24:47 -0700

    Refactor styling and layout in AddPipelinePage.tsx

--- src/pages/ResourceManger/AddPipelinePage.tsx
+++ src/pages/ResourceManger/AddPipelinePage.tsx
@@ -4,10 +4,10 @@ interface Props{};
 
 const AddPipelinegPage = (props:Props)=>{
   return(<>
-  <div className="flex h-screen bg-gray-100">
+  <div className="">
 
     {/* Main Content */}
-    <div className="flex-grow">
+    <div className="flex-flex-row">
       <div className="px-5 pt-4 d-flex mb-3">
         <div className="p-2 me-auto">
           <h1> New Pipeline </h1>
@@ -16,28 +16,122 @@ const AddPipelinegPage = (props:Props)=>{
 
       <div className='ml-10 mr-10 p-4 d-flex justify-content-between border-top border-dark'>
         {/* NO CHANGES */}
-        <div className="flex flex-col">
-          <div>
-            img
+        <div className="flex flex-col ml-20">
+          <div className=" mb-3">
+            <div className="container bg-gray-200 rounded">img</div>
           </div>
 
           {/* URL */}
-          <div className="flex flex-row">
+          <div className="flex flex-row mb-3">
             <input className="p-2 pl-0 w-full text-sm bg-white" placeholder="Url " />
             <button className="btn btn-secondary">Browse</button>
           </div>   
 
           {/* Img Workforce */}
-          <div className="flex flex-wrap ">
-            <div></div>
-            <div></div>
-            <div></div>
+          <div className="flex flex-col">
 
+            <div className="flex flex-row mb-3">
+              <div className="container bg-blue-300 rounded text-left">
+                <p>Id</p>
+              </div>
+              <div className="container bg-gray-200 rounded text-left">
+                <p>A01253056</p>
+              </div>
+            </div>
+
+            <div className="flex flex-row">
+            <div className="container bg-blue-300 rounded text-left">
+                <p>Joining Date</p>
+              </div>
+              <div className="container bg-gray-200 rounded text-left">
+                <p>12/12/2023</p>
+              </div>
+            </div>
           </div>       
         </div>
 
         {/* AVAILABLE TO EDIT */}
-        <div>
+
+        <div className="flex flex-row ">
+
+          <div className="flex flex-col mr-6">
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Name</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Gender</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Jon Title</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Propose Action</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Skills</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+          </div>
+
+        </div>
+        
+        <div className="flex flex-row ">
+
+          <div className="flex flex-col mr-6">
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Email</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Division</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Tech Stack</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Reason Current State</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-row ">
+
+          <div className="flex flex-col mr-6">
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Phone</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Job Grade</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Employee Status</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+
+            <div className="flex flex-col mb-6">
+              <p className="text-left">Salary</p>
+              <input className="p-2 pl-0 w-full text-sm bg-white border border-gray-300 rounded-md" placeholder="Name " />
+            </div>
+          </div>
 
         </div>
       </div>
