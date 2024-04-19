92a91103426956e8548d30c0ce9766440cd3f541
danielagallardoc <a01252869@tec.mx>
2024-04-19 09:08:32 -0700

    changes

---
+++ src/pages/Account Manager/Projects/DeleteModalProject.tsx
@@ -0,0 +1,13 @@
+
+interface Props {
+    isActive: boolean;
+    selectedId: number;
+    setDeleteActive: Function;
+}
+
+const ProjectDelete = (props: Props) => {
+    
+
+};
+
+export default ProjectDelete;
