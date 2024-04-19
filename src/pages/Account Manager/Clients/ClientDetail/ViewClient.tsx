5e7bbf0decaa6b7f396cb0eb48c095ca539bc05b
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-19 03:38:21 -0700

    Clien content section finishec

--- src/pages/Account Manager/Clients/ClientDetail/ViewClient.tsx
+++ src/pages/Account Manager/Clients/ClientDetail/ViewClient.tsx
@@ -29,72 +29,88 @@ const ViewClient = () => {
   const [currentClient] = useOutletContext<[Client | null]>();
   return (
     <div className="main-content-view-client">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
         {/* Left Side */}
-        <div className="md:col-span-2 p-6 bg-white rounded-lg shadow-lg">
-          <h1>Marketing Manager</h1>
-          <h2>Job Description</h2>
-          <p>
-            Occaecati est et illo quibusdam accusamus qui. Incidunt aut et
-            molestiae ut facere aut. Est quidem iusto praesentium excepturi
-            harum nihil tenetur facilis. Ut omnis voluptates nihil accusantium
-            doloribus eaque debitis.
-          </p>
-          <h2>Key Responsibilities</h2>
-          <ul>
-            <li>
-              Working with agency for design drawing detail, quotation and local
-              production.
-            </li>
-            <li>
-              Produce window displays, signs, interior displays, floor plans and
-              special promotions displays
-            </li>
-            <li>
-              Change displays to promote new product launches and reflect
-              festive or seasonal themes.
-            </li>
-            <li>
-              Planning and executing the open/renovation/ closing store
-              procedure
-            </li>
-            <li>
-              Follow‐up store maintenance procedure and keep updating SKU In &
-              Out.
-            </li>
-            <li>Monitor costs and work within budget.</li>
-            <li>Liaise with suppliers and source elements.</li>
-          </ul>
+        <div className="md:col-span-8 p-6 bg-white rounded-lg shadow-lg">
+          <div className="details-section-view-client">
+            <h1 className="client-name-view-client">{currentClient?.name}</h1>
+            <h2 className="client-description-title">Client Description</h2>
+            <p className="client-description-text">
+              Occaecati est et illo quibusdam accusamus qui. Incidunt aut et
+              molestiae ut facere aut. Est quidem iusto praesentium excepturi
+              harum nihil tenetur facilis. Ut omnis voluptates nihil accusantium
+              doloribus eaque debitis.
+            </p>
+            <h2 className="client-key-title">Key Responsibilities</h2>
+            <ul className="custom-bullet">
+              <li className="list-item">
+                Working with agency for design drawing detail, quotation and
+                local production.
+              </li>
+              <li className="list-item">
+                Produce window displays, signs, interior displays, floor plans
+                and special promotions displays
+              </li>
+              <li className="list-item">
+                Change displays to promote new product launches and reflect
+                festive or seasonal themes.
+              </li>
+              <li className="list-item">
+                Planning and executing the open/renovation/ closing store
+                procedure
+              </li>
+              <li className="list-item">
+                Follow‐up store maintenance procedure and keep updating SKU In &
+                Out.
+              </li>
+              <li className="list-item">
+                Monitor costs and work within budget.
+              </li>
+              <li className="list-item">
+                Liaise with suppliers and source elements.
+              </li>
+            </ul>
 
-          <h2>Why You'll Love Working Here</h2>
-          <ul>
-            <li>
-              Working with agency for design drawing detail, quotation and local
-              production.
-            </li>
-            <li>
-              Produce window displays, signs, interior displays, floor plans and
-              special promotions displays
-            </li>
-            <li>
-              Change displays to promote new product launches and reflect
-              festive or seasonal themes.
-            </li>
-            <li>
-              Planning and executing the open/renovation/ closing store
-              procedure
-            </li>
-            <li>
-              Follow‐up store maintenance procedure and keep updating SKU In &
-              Out.
-            </li>
-            <li>Monitor costs and work within budget.</li>
-            <li>Liaise with suppliers and source elements.</li>
-          </ul>
+            <h2 className="client-why-title">Why You'll Love Working Here</h2>
+            <ul className="custom-bullet">
+              <li className="list-item">
+                Working with agency for design drawing detail, quotation and
+                local production.
+              </li>
+              <li className="list-item">
+                Produce window displays, signs, interior displays, floor plans
+                and special promotions displays
+              </li>
+              <li className="list-item">
+                Change displays to promote new product launches and reflect
+                festive or seasonal themes.
+              </li>
+              <li className="list-item">
+                Planning and executing the open/renovation/ closing store
+                procedure
+              </li>
+              <li className="list-item">
+                Follow‐up store maintenance procedure and keep updating SKU In &
+                Out.
+              </li>
+              <li className="list-item">
+                Monitor costs and work within budget.
+              </li>
+              <li className="list-item">
+                Liaise with suppliers and source elements.
+              </li>
+            </ul>
+            <h2 className="client-benefits-title">Benefits</h2>
+            <div className="benefits">
+              <span className="benefit-tag">Free parking</span>
+              <span className="benefit-tag">Bonus commission</span>
+              <span className="benefit-tag">Travel</span>
+            </div>
+          </div>
         </div>
 
         {/* Right Side */}
-        <div className="space-y-6">
+        <div className="md:col-span-4 space-y-6">
           {/* Details Box */}
           <div className="p-6 bg-white rounded-lg shadow-lg">
             <div className="date-posted">
@@ -173,7 +189,7 @@ const ViewClient = () => {
               <div className="right-card-section">
                 <h4 className="owners-view-client">Lueilwitz and Sons</h4>
                 <p className="client-direction-view-client">
-                  19034 Verna Unions Apt. 164 - Honolulu, RI / 87535
+                  19034 Verna Unions Apt. 164 - <br></br> Honolulu, RI / 87535
                 </p>
                 <p className="client-telephone-view-client">365-374-4961</p>
               </div>
