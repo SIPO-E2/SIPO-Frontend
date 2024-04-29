8561684a6ecd421f21d130253fabf2b7c8b348b4
HectorGtz27 <alan.gtz51@gmail.com>
2024-04-29 10:10:45 -0700

    Implement date range picker in RoleUserList component

--- src/pages/Account Manager/RolesUser/RoleUserList.tsx
+++ src/pages/Account Manager/RolesUser/RoleUserList.tsx
@@ -8,6 +8,18 @@ import {
   faChevronUp,
   faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
+/* --------------------- IMPORTING DATE LIBRARY --------------------- */
+import { DateRangePicker, RangeKeyDict } from "react-date-range";
+import { addDays } from "date-fns";
+import "react-date-range/dist/styles.css"; // Estilos principales
+import "react-date-range/dist/theme/default.css"; // Tema por defecto
+import { format } from "date-fns";
+
+interface DateRange {
+  startDate?: Date; // La fecha de inicio puede ser Date o undefined
+  endDate?: Date; // La fecha de finalización puede ser Date o undefined
+  key: string;
+}
 
 const RoleUserList = () => {
   /* --------------------- MODAL --------------------- */
@@ -17,6 +29,41 @@ const RoleUserList = () => {
     setIsModalOpen(!isModalOpen);
   };
 
+  /* --------------------- DATE RANGE --------------------- */
+
+  // Estado para las fechas del rango
+  const [state, setState] = useState<DateRange[]>([
+    // Asegurando que el estado cumpla con el tipo DateRange
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
+
+  const [dateRangeText, setDateRangeText] = useState(
+    `${format(new Date(), "MMM dd, yyyy")} - ${format(
+      addDays(new Date(), 7),
+      "MMM dd, yyyy"
+    )}`
+  );
+
+  // Estado para la fecha seleccionada que se mostrará en la interfaz de usuario
+  const [selectedRange, setSelectedRange] = useState<DateRange>({
+    key: "selection",
+  });
+
+  const applyDateSelection = () => {
+    // Actualizar el texto para mostrar en la interfaz de usuario
+    if (selectedRange.startDate && selectedRange.endDate) {
+      const newDateRangeText = `${format(
+        selectedRange.startDate,
+        "MMM dd, yyyy"
+      )} - ${format(selectedRange.endDate, "MMM dd, yyyy")}`;
+      setDateRangeText(newDateRangeText);
+    }
+    toggleModal(); // Cerrar el modal
+  };
   /* --------------------- Fetch Roles --------------------- */
 
   const { roles, fetchRoles } = useApisStore();
@@ -40,6 +87,7 @@ const RoleUserList = () => {
           </div>
 
           <div className="roles-top-filters">
+            <p onClick={toggleModal}>{dateRangeText}</p>
             <p>Select Date</p>
             <div onClick={toggleModal}>
               <FontAwesomeIcon
@@ -49,12 +97,32 @@ const RoleUserList = () => {
             </div>
           </div>
 
-          {/* Modal */}
           {isModalOpen && (
             <div className="modal-roles">
               <div className="modal-content-roles">
-                {/* Contenido del modal */}
-                <button onClick={toggleModal}>Close</button>
+                <DateRangePicker
+                  onChange={(item: RangeKeyDict) => {
+                    // Actualiza el estado que usa el componente DateRangePicker
+                    const newRanges = [
+                      {
+                        ...item.selection,
+                        key: "selection",
+                      },
+                    ];
+                    setState(newRanges);
+                    // También actualiza el rango seleccionado que se utilizará cuando se haga clic en Apply
+                    setSelectedRange(newRanges[0]);
+                  }}
+                  // Removiendo las propiedades que no son reconocidas o soportadas
+                  months={2}
+                  ranges={state}
+                  direction="horizontal"
+                />
+                {/* Botones */}
+                <div>
+                  <button onClick={toggleModal}>Cancel</button>
+                  <button onClick={applyDateSelection}>Apply</button>
+                </div>
               </div>
             </div>
           )}
