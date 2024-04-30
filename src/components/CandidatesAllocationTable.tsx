cf36d671d08d174fc6fc83800ec7b24d079c30b3
umakossiooo <a01254619@tec.mx>
2024-04-30 01:14:56 -0700

    refactor: deleted unnecesary commented code

--- src/components/CandidatesAllocationTable.tsx
+++ src/components/CandidatesAllocationTable.tsx
@@ -188,10 +188,6 @@ const CandidatesAllocationTable = () => {
                                 const client = clients.find((client) => client.id === allocation.client_id);
                                 const jobPosition = jobPositions.find((jobPosition) => jobPosition.id === allocation.jobPositionId);
 
-                                // const hasActiveInterview = allocation.interviews.some(interview => interview.activeDB);
-                                // const allocationStatus = hasActiveInterview ? AllocationStatus.ClientInterview : AllocationStatus.Allocated;
-                                // updateAllocation(allocation.id.toString(), allocationStatus)
-
                                 const interviewDate = selectedDateMap[allocation.id] || '';
                                 const interviewReasonStatus = reasonStatusMap[allocation.id] || '';
                                 const interviewStatus = interviewStatusMap[allocation.id] || InterviewStatus.Scheduled;
