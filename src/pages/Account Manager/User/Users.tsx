6d417bc110ce342f47dbfe3da7caef2c4cb4cf21
HectorGtz27 <alan.gtz51@gmail.com>
2024-05-03 01:00:31 -0700

    Add pagination and search functionality to Users page

--- src/pages/Account Manager/User/Users.tsx
+++ src/pages/Account Manager/User/Users.tsx
@@ -1,17 +1,42 @@
 // User.tsx
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useApisStore } from "../../../store/apiStore";
+import Pagination from "../../../components/Pagination";
+import { Link } from "react-router-dom";
 
 const Users = () => {
-  const { users, fetchUsers } = useApisStore();
+  const { users, fetchUsers, setUsers } = useApisStore((state) => ({
+    users: state.users,
+    fetchUsers: state.fetchUsers,
+    setUsers: state.setUsers,
+  }));
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
+  const itemsPerPage = 12;
 
   useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
+    fetchUsers(currentPage, itemsPerPage, searchQuery, true);
+  }, [currentPage, searchQuery, fetchUsers]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1); // Ensuresss a fresh search from page 1
+  };
 
   return (
     <div>
-      <h1>Users and Their Clients</h1>
+      <h1>Users</h1>
+      <div className="search-section-users">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search for clients..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </div>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
@@ -29,6 +54,12 @@ const Users = () => {
           </li>
         ))}
       </ul>
+      <Pagination
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        totalItems={totalItems}
+        paginate={setCurrentPage}
+      />
     </div>
   );
 };
